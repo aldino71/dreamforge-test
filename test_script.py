aeb3d5d90c5352ba2f70fdafc18ecd66d8f50ddf
Dino K <dinokeserovic@hotmail.com>
2023-11-18 16:51:51 -0600

    second run, need to restart for env vars?

--- test_script.py
+++ test_script.py
@@ -1,4 +1,91 @@
-### Start of Script ###
+"""
+Module/Script Name: test_script.py
+Description: Test bed for functions, api calls, and preprocessing information
+
+Usage:
+    - Provide information on how to use the script or module.
+    - Include examples of command-line arguments or function calls.
+
+Requirements:
+    - numpy
+    - pandas
+    - openai
+    - sqlite
+    - sqlalchemy
+
+Author: Dino Keserovic
+Created: 11/17/2023
+"""
+
+#imports
 import numpy
 import pandas
-print("Hello World!")
+from openai import OpenAI
+import requests
+import argparse
+import os
+
+#set and define global variables
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+#Function to execute API calls
+def execute_api_call(api_url, parameters=None):
+    """
+    Execute an API call.
+
+    Args:
+        api_url (str): The URL of the API endpoint.
+        parameters (dict): Optional parameters for the API call.
+
+    Returns:
+        dict: The API response in JSON format.
+    """
+    # Your API call implementation here using requests library
+    response = requests.get(api_url, params=parameters)
+    return response.json()
+
+# Function to execute database queries
+def execute_database_query(query):
+    """
+    Execute a database query.
+
+    Args:
+        query (str): The SQL query to be executed.
+
+    Returns:
+        list: The result of the database query.
+    """
+    # Your database query execution implementation here
+    # Use appropriate database library (e.g., SQLite, SQLAlchemy)
+
+# Main function for script execution
+def main():
+    """
+    Main function for script execution.
+
+    Usage:
+        python YourModuleName.py --arg1 value1 --arg2 value2
+    """
+    # Argument parsing
+    parser = argparse.ArgumentParser(description='Description of the script.')
+    parser.add_argument('--arg1', type=str, help='Description of argument 1.')
+    parser.add_argument('--arg2', type=int, help='Description of argument 2.')
+
+    # Parse command-line arguments
+    args = parser.parse_args()
+
+    # Your script logic here
+    # Call functions, execute queries, and make API calls based on user inputs
+
+# Execute the script if it's run as the main module
+#if __name__ == "__main__":
+#    main()
