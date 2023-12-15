d0dd5df0e6e12f2c7b264ae1a2d4c3c0b4da4094
Dino K <dino.keserovic@hotmail.com>
2023-12-15 15:11:31 -0600

    moving header and images

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,14 @@
 // static/js/app.js
 console.log('Hello, D&D Chatbot!');
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
