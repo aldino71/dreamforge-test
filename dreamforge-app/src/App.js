17f1c12bbb66c9de7dee291e4b71c4e35285363a
Dino K <dino.keserovic@hotmail.com>
2023-12-15 13:39:42 -0600

    integrate react

---
+++ dreamforge-app/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to D&D Chatbot</h1>
+      {/* Add your React components and logic here */}
+    </div>
+  );
+}
+
+export default App;
